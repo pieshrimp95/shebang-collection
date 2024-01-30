9c9a42cedd2a39febaf17c0f2b69173e505bd0f9
wheatprune98 <157942791+wheatprune98@users.noreply.github.com>
2024-01-30 21:13:20 +0700

    Overwrite update.sh with new script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,38 +1,42 @@
 #! /bin/sh
 # Update.sh is an interactive clean update formed in Debian 12 Bookworm
-# ======= Variables Section =======
-# EMPTY (Fill this with variable)
 # ======= First Section =======
-echo "Welcome to Debian 12 Bookworm Interactive Updater"
+echo "Welcome to \033[1mDebian 12 Bookworm Interactive Updater\033[0m"
 echo "Updater will automatically detects and updates any package wihout confirmation with auto-removal for unused packages"
 read -r -p "Are you sure you want to continue? Theres no way back after this. (y/n): " answer
 # ======= Second Section =======
 if [ "$answer" = "y" ]; then
     echo "Starting..."
         # Update Code goes here
-        sudo apt update >> log.txt
-        sudo apt upgrade >> log.txt
-        sudo apt autoremove >> log.txt
-        sudo apt-get purge >> log.txt
-        sudo apt apt-get clean >> log.txt
-        sudo apt-get autoclean >> log.txt
+        sudo apt-get update |tee logs.txt
+        sudo apt-get upgrade |tee logs.txt
+        sudo apt-get autoremove | tee logs.txt
+        sudo apt-get purge | tee logs.txt
+        sudo apt-get clean | tee logs.txt
+        sudo apt-get autoclean | tee logs.txt
         # Aftermath 
         echo "Update has finished and ready-to-go"
         read -r -p "Would you read the logs? (y/n): " aftermath
  else
-    echo "Answer not valid, Aborting..."
     echo "Press Enter to Close"
     read dummy
+    exit 1
 fi
 
+# ======= Aftermath Section =======
 if [ "$aftermath" = "y" ]; then
-    open log.txt
-    echo "Aborting..."
-    echo "Press Enter to close..."
+    open logs.txt
+    echo "Text editor has closed."
+    echo "Press Enter to close"
     read dummy
+    exit 0
+ else
+    echo "Press Enter to close"
+    read dummy
+    exit 1
 fi
 
 # Finished reading the code? Don't forget to rate and feedback me on github!
-# Link to github: 
-# Made by (USER), Scripts reserved to the author.
+# Link to github: https://github.com/wheatprune98/shscripts/
+# Made by wheatprune98, Scripts licensed with GNU General Public License 3.0 (see LICENSE)
 # END OF THE CODE
